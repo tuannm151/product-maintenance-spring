fec33f3d5c7c5939ea16714f015ac57ac116d7f5
Minh Tuan <tuanxsokoh@gmail.com>
2022-05-07 09:18:05 +0700

    Added template

--- src/main/java/com/example/productmtspring/model/Product.java
+++ src/main/java/com/example/productmtspring/model/Product.java
@@ -1,20 +1,17 @@
 package com.example.productmtspring.model;
 
-import lombok.Data;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-import lombok.Setter;
+import lombok.*;
+
+import java.math.BigDecimal;
 
 @Data
 @RequiredArgsConstructor
+@NoArgsConstructor
 @Getter
 @Setter
 public class Product {
 
     private String code;
     private String description;
-    private double price;
-
-
-
+    private BigDecimal price;
 }
