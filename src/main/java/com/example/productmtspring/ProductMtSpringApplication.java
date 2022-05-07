ca7537bc4930603055ce8bbe20e517e98da13a9a
Minh Tuan <tuanxsokoh@gmail.com>
2022-05-07 08:25:48 +0700

    Initial commit

---
+++ src/main/java/com/example/productmtspring/ProductMtSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.productmtspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductMtSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductMtSpringApplication.class, args);
+    }
+
+}
