466ca6b3e3e42b7d6e0a50d2047b591d219fa86b
Minh Tuan <tuanxsokoh@gmail.com>
2022-05-07 11:21:56 +0700

    Added templates

--- src/main/java/com/example/productmtspring/controller/ProductController.java
+++ src/main/java/com/example/productmtspring/controller/ProductController.java
@@ -2,12 +2,10 @@ package com.example.productmtspring.controller;
 
 import com.example.productmtspring.model.Product;
 import com.example.productmtspring.repository.ProductRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+
 
 import java.util.ArrayList;
 
